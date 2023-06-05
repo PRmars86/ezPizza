d8bedca8ad25d67cfc568d38eaf52cc624f02e61
PRmars86 <pengruima@gmail.com>
2023-06-05 13:38:52 -0700

    add nav bar

---
+++ pages/_app.js
@@ -0,0 +1,29 @@
+import "../components/globals.css"
+import Link from 'next/link';
+import styles from '../components/MyApp.module.css'
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <nav className={styles.navbar}>
+                <ul className={styles.navbarContactBox}>
+                    <Link href="/" >
+                        <div className={styles.brandPizza}>
+                            <img src='/assets/images/pizza-slice.png' alt='brand'></img>
+                            <p>Pizza shop</p>
+                        </div>
+                    </Link>
+                    <Link href="/" className={styles.navbarContact}>
+                        Home
+                    </Link>
+                    <Link href="/about" className={styles.navbarContact}>
+                        About
+                    </Link>
+                </ul>
+            </nav>
+            <div>
+                <Component {...pageProps} />
+            </div>
+        </div>
+    )
+}
